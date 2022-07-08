d0474e5885c3697797b20efe790bbd89d7f1a8e1
Victor Olala <olalavictor01@gmail.com>
2022-07-08 14:55:55 +0300

    Update minet.js

--- minet.js
+++ minet.js
@@ -1,28 +1,29 @@
+
 var currentDate = new Date();
 var expiryDate = new Date("09/24/2022 11:36:00 AM");
-// expiryDate.setMinutes(expiryDate.getMinutes() + 3);
-if (currentDate >= expiryDate) {
-    var opacity = 0;
-    var intervalID = 0;
-    window.onload = fadeout;
 
-    function fadeout() {
-        setInterval(hide, 50);
-    }
+if(currentDate>=expiryDate){
+   	var opacity=0;
+	var intervalID=0;
+	window.onload=fadeout;
+		function fadeout(){
+			setInterval(hide, 50);
+		}
+	function hide(){
+		var body=document.getElementById("body");
+		opacity =
+Number(window.getComputedStyle(body).getPropertyValue("opacity"))
 
-    function hide() {
-        var body = document.getElementById("body");
-        opacity =
-            Number(window.getComputedStyle(body).getPropertyValue("opacity"))
+			if(opacity>0){
+				opacity=opacity-0.1;
+						body.style.opacity=opacity
+			}
+			else{
+				clearInterval(intervalID);
+			}
+		}
+        console.log('Time Out, Contact Developer @ olalavictor01@gmail.com');
+}else{
 
-        if (opacity > 0) {
-            opacity = opacity - 0.1;
-            body.style.opacity = opacity
-        } else {
-            clearInterval(intervalID);
-        }
-    }
-} else {
-
-    console.log(expiryDate);
+console.log(expiryDate);
 }

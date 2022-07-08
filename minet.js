02d86733bcadaff1cf8ce51c65d0cec6846cdc21
Victor Olala <olalavictor01@gmail.com>
2022-07-08 14:49:03 +0300

    Create minet.js

---
+++ minet.js
@@ -0,0 +1,28 @@
+var currentDate = new Date();
+var expiryDate = new Date("09/24/2022 11:36:00 AM");
+// expiryDate.setMinutes(expiryDate.getMinutes() + 3);
+if (currentDate >= expiryDate) {
+    var opacity = 0;
+    var intervalID = 0;
+    window.onload = fadeout;
+
+    function fadeout() {
+        setInterval(hide, 50);
+    }
+
+    function hide() {
+        var body = document.getElementById("body");
+        opacity =
+            Number(window.getComputedStyle(body).getPropertyValue("opacity"))
+
+        if (opacity > 0) {
+            opacity = opacity - 0.1;
+            body.style.opacity = opacity
+        } else {
+            clearInterval(intervalID);
+        }
+    }
+} else {
+
+    console.log(expiryDate);
+}
